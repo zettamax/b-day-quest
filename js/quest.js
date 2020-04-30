14d372183d879f19deabf0e025145bd7dba39bd7
max <zettamax@gmail.com>
2020-04-30 14:08:09 +0300

    Updated

--- js/quest.js
+++ js/quest.js
@@ -18,6 +18,7 @@ $(function () {
         },
         {
             name: "Задание первое",
+            img_src: "https://i.timeout.ru/pix/resize/561/558/750x485.png",
             title: "Под носом",
             summary: "То, что тебе надо, у тебя сейчас в руках. Найди задание и реши",
             button: "Я знаю ответ!",
@@ -25,20 +26,23 @@ $(function () {
         },
         {
             name: "Задание второе",
+            img_src: "https://i.pinimg.com/564x/ae/17/28/ae17283c3d2cb7a3e9298a05b3a7c117.jpg",
             title: "Эй, Тони",
             summary: "Открой панду!",
             button: "Вперед!",
-            result: "1"
+            result: "9217"
         },
         {
             name: "Задание третье",
+            img_src: "https://i.ytimg.com/vi/C5zNATDTsCQ/hqdefault.jpg",
             title: "В темноте",
             summary: "Следуюший код ты найдешь в самой большой темной комнате, но только при лучах ИНОГО света",
             button: "Нашла!",
-            result: "1"
+            result: "4587"
         },
         {
             name: "Задание четвертое",
+            img_src: "https://dz1ambdcpmi0e.cloudfront.net/drink-cans.png",
             title: "Михалыч, это жесть",
             summary: "Иди на балкон и приберись там - это поможет найти подсказку",
             button: "Фух!",
@@ -46,6 +50,7 @@ $(function () {
         },
         {
             name: "Задание пятое",
+            img_src: "https://neftegaz.ru/upload/iblock/30f/30f4fe4f12dc32bd33370597ccb10975.jpg",
             title: "Пораскинь мозгами",
             summary: "Ответом для этого задания будет самый большой атомный номер природного изотопа урана",
             button: "Я умею гуглить!",
@@ -53,6 +58,7 @@ $(function () {
         },
         {
             name: "Задание шестое, не последнее",
+            img_src: "swsw0swsw-sww",
             title: "Пораскинь мозгами #2",
             summary: "",
             button: "Я умею гуглить!",
@@ -60,6 +66,7 @@ $(function () {
         },
         {
             name: "Задание седьмое",
+            img_src: "https://wallbox.ru/wallpapers/preview/201532/08f08c01394311f.jpg",
             title: "Арт-терапия",
             summary: "Отдыхая у реки, ты найдешь ответ",
             button: "Отдохнула!",
@@ -67,6 +74,7 @@ $(function () {
         },
         {
             name: "Задание восьмое",
+            img_src: "https://novate.ru/files/u36260/plaxkier12dsa.jpg",
             title: "Strange things",
             summary: "Ответ спрятан в диковинной вещи в обители единорогов",
             button: "Ну вы даете!",
@@ -74,6 +82,7 @@ $(function () {
         },
         {
             name: "Задание девятое",
+            img_src: "https://zakon.ru/Content/entriesattachments/6567d90b-5012-4538-80df-67acb8d1c7f8.jpg",
             title: "Штампы, штампы, штампы",
             summary: "Ты пропесочила эту комнату, теперь тебе следует ее опечатать",
             button: "Ну вы даете!",
@@ -81,10 +90,11 @@ $(function () {
         },
         {
             name: "Десяточка",
+            img_src: "https://pro-alko.ru/wp-content/uploads/2019/03/Viski_sauer_klassicheskiy_i_originalnye_recepty_kokteylya__sostav_i_proporcii_1.jpg",
             title: "Фух! Вот это жаришка!",
             summary: "Пора бы нам охладиться. Ответ спрятан В ТОМ, без чего не обойдется ни один коктейль",
             button: "Нашла!",
-            result: "6713"
+            result: "7144"
         },
         {
             name: "Финиш!",
